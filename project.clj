4a01d61cc217947ba9ff96e054e671bb8f63b87e
Thomas Crowley <crowley@kibu.com.au>
2015-08-30 16:02:37 +1000

    Bump deps

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3211"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.48"]]
 
   :aliases {"deploy" ["do" "clean," "deploy" "clojars"]
             "test" ["do" "clean," "with-profile" "dev" "cljsbuild" "test"]}
