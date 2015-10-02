881cb5e5ab3fbc0be5c5361d8f0847a7b2cf7635
Thomas Crowley <crowley@kibu.com.au>
2015-10-02 12:51:36 +1000

    Bump to 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kibu/pushy "0.3.3"
+(defproject kibu/pushy "0.3.4"
   :description "HTML5 pushState for Clojurescript"
   :url "https://github.com/kibu-australia/pushy"
   :license {:name "Eclipse Public License"
